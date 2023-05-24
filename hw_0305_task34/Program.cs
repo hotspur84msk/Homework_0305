25b666f870c7ac7cffd8d8640e0dd6f5024760ee
Sergei.Petrushin <sjabrail@gmail.com>
2023-05-24 18:44:10 +0530

    program is done

---
+++ hw_0305_task34/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+Console.WriteLine("Введите длину массива ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+void RandomArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100, 999);
+    }
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+    Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+int CountEvenElements(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count = count + 1;
+        }
+    }
+    return count;
+}
+
+
+int[] array = new int[a];
+RandomArr(array);
+PrintArray(array);
+Console.WriteLine(CountEvenElements(array));
